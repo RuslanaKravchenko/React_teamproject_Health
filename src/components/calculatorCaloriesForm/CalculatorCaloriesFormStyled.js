ea78e78a58449c67ec805c00dc15e6e86696af22
Ruslana Kravchenko <ruslana.kravchenko24@gmail.com>
2021-02-23 13:59:34 +0200

    added padding in CalculatorCaloriesForm

--- src/components/calculatorCaloriesForm/CalculatorCaloriesFormStyled.js
+++ src/components/calculatorCaloriesForm/CalculatorCaloriesFormStyled.js
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 
 export const FormContainer = styled.div`
     .caloriesForm {
+        padding-bottom: 40px;
         @media (max-width: 767px) {
             margin: 0 auto;
             width: 100%;
